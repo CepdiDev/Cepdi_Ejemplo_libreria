932462c8257bd86252ba42cb736249e6c66c14ed
Francisco Gomez <francisco.gomez@cepdi.mx>
2021-07-12 14:55:19 -0500

    Se agrega proyecto .net framework para test de apis y se elimina proyecto .net core

---
+++ TestWebApiREST/Program.cs
@@ -0,0 +1,137 @@
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWebApiREST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string UrlEntrada = ConfigurationManager.AppSettings["Entrada"];
+            string Usuario = ConfigurationManager.AppSettings["Usuario"];
+            string Password = ConfigurationManager.AppSettings["Password"];
+
+            string Token = ValidarAcceso(Usuario, Password);
+
+            if (Token != null)
+            {
+                string[] ListaArchivos = Directory.GetFiles(UrlEntrada, "*.*", SearchOption.AllDirectories);
+
+                foreach (var archivo in ListaArchivos)
+                {
+                    string UrlCarpetaSalida = archivo.Replace("Entrada", "Salida");
+                    string UrlXML = UrlCarpetaSalida + "/Comprobante.xml";
+                    string UrlLog = UrlCarpetaSalida + "/Log.txt";
+                    Directory.CreateDirectory(UrlCarpetaSalida);
+                    File.WriteAllText(UrlLog, ".- Inicia timbrado");
+
+                    try
+                    {
+                        string Layout = File.ReadAllText(archivo);
+
+                        //Timbrar Archivo
+                        var respuestaTimbrado = TimbrarComprobante(Layout, Usuario, Password, Token);
+                        if (respuestaTimbrado.GetValue("exitoso").ToString() == "true")
+                        {
+                            using (StreamWriter Log = new StreamWriter(UrlLog, true))
+                            {
+                                Log.WriteLine(".- Timbrado con exito");
+                                Log.Close();
+                            }
+                            File.WriteAllText(UrlXML, respuestaTimbrado.GetValue("XmlTimbrado").ToString());
+                            using (StreamWriter Log = new StreamWriter(UrlLog, true))
+                            {
+                                Log.WriteLine(".- Se crea xml en la carpeta");
+                                Log.Close();
+                            }
+                        }
+                        else
+                        {
+                            using (StreamWriter Log = new StreamWriter(UrlLog, true))
+                            {
+                                Log.WriteLine(".- Error al timbrar");
+                                Log.WriteLine(".- Mensaje: " + respuestaTimbrado.GetValue("mensajeError").ToString());
+                                Log.WriteLine(".- Codigo Error: " + respuestaTimbrado.GetValue("codigoError").ToString());
+                                Log.Close();
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        using (StreamWriter Log = new StreamWriter(UrlLog, true))
+                        {
+                            Log.WriteLine(".- Se produjo una Exception");
+                            Log.WriteLine(".- Mensaje: " + e.Message);
+                            Log.Close();
+                        }
+                    }
+
+                }
+            }
+        }
+
+
+        public static string ValidarAcceso(string Usuario, string Password)
+        {
+            var client = new RestClient(ConfigurationManager.AppSettings["UrlValidarAcceso"]);
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            request.AlwaysMultipartFormData = true;
+            request.AddParameter("usuario", Usuario);
+            request.AddParameter("password", Password);
+            IRestResponse response = client.Execute(request);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var Json = JObject.Parse(response.Content);
+                string token = Json.GetValue("token").ToString();
+                return token;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static JObject TimbrarComprobante(string Layout, string Usuario, string Password, string Token)
+        {
+            var client = new RestClient(ConfigurationManager.AppSettings["UrlTimbrarComprobante"]);
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Authorization", "Bearer " + Token);
+            request.AlwaysMultipartFormData = true;
+            request.AddParameter("usuario", Usuario);
+            request.AddParameter("password", Password);
+            request.AddParameter("texto", Layout);
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+
+            return JObject.Parse(response.Content);
+        }
+
+        public static JObject ObtenerPDFComprobante(string UUID, string Usuario, string Password, string Token)
+        {
+            var client = new RestClient(ConfigurationManager.AppSettings["UrlObtenerPDFComprobante"]);
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Authorization", "Bearer " + Token);
+            request.AlwaysMultipartFormData = true;
+            request.AddParameter("usuario", Usuario);
+            request.AddParameter("password", Password);
+            request.AddParameter("uuid", UUID);
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+
+            return JObject.Parse(response.Content);
+        }
+    }
+}
