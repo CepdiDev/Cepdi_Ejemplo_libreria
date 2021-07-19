c49d7f6a8e9a51b6735ce3d12a3b365917d58255
Francisco Gomez <francisco.gomez@cepdi.mx>
2021-07-19 11:22:14 -0500

    Se agrega TEST de KAVAK

--- TestWebApiREST/Program.cs
+++ TestWebApiREST/Program.cs
@@ -22,78 +22,82 @@ namespace TestWebApiREST
             string Usuario = ConfigurationManager.AppSettings["Usuario"];
             string Password = ConfigurationManager.AppSettings["Password"];
 
-            string Token = ValidarAcceso(Usuario, Password);
+            //string Token = ValidarAcceso(Usuario, Password);
 
-            if (Token != null)
+            //if (Token != null)
+            //{
+            //Timbrar Documentos
+            string[] ListaArchivos = Directory.GetFiles(UrlEntrada, "*.*", SearchOption.AllDirectories);
+
+            foreach (var archivo in ListaArchivos)
             {
-                //Timbrar Documentos
-                //string[] ListaArchivos = Directory.GetFiles(UrlEntrada, "*.*", SearchOption.AllDirectories);
+                string UrlCarpetaSalida = archivo.Replace("Entrada", "Salida");
+                string UrlXML = UrlCarpetaSalida + "/Comprobante.xml";
+                string UrlLog = UrlCarpetaSalida + "/Log.txt";
+                Directory.CreateDirectory(UrlCarpetaSalida);
+                File.WriteAllText(UrlLog, ".- Inicia timbrado");
 
-                //foreach (var archivo in ListaArchivos)
-                //{
-                //    string UrlCarpetaSalida = archivo.Replace("Entrada", "Salida");
-                //    string UrlXML = UrlCarpetaSalida + "/Comprobante.xml";
-                //    string UrlLog = UrlCarpetaSalida + "/Log.txt";
-                //    Directory.CreateDirectory(UrlCarpetaSalida);
-                //    File.WriteAllText(UrlLog, ".- Inicia timbrado");
+                try
+                {
+                    string Layout = File.ReadAllText(archivo);
 
-                //    try
-                //    {
-                //        string Layout = File.ReadAllText(archivo);
+                    //Timbrar Archivo
+                    //var respuestaTimbrado = TimbrarComprobante(Layout, Usuario, Password, Token);
 
-                //        //Timbrar Archivo
-                //        var respuestaTimbrado = TimbrarComprobante(Layout, Usuario, Password, Token);
-                //        if (respuestaTimbrado.GetValue("exitoso").ToString() == "true")
-                //        {
-                //            using (StreamWriter Log = new StreamWriter(UrlLog, true))
-                //            {
-                //                Log.WriteLine(".- Timbrado con exito");
-                //                Log.Close();
-                //            }
-                //            File.WriteAllText(UrlXML, respuestaTimbrado.GetValue("XmlTimbrado").ToString());
-                //            using (StreamWriter Log = new StreamWriter(UrlLog, true))
-                //            {
-                //                Log.WriteLine(".- Se crea xml en la carpeta");
-                //                Log.Close();
-                //            }
-                //        }
-                //        else
-                //        {
-                //            using (StreamWriter Log = new StreamWriter(UrlLog, true))
-                //            {
-                //                Log.WriteLine(".- Error al timbrar");
-                //                Log.WriteLine(".- Mensaje: " + respuestaTimbrado.GetValue("mensajeError").ToString());
-                //                Log.WriteLine(".- Codigo Error: " + respuestaTimbrado.GetValue("codigoError").ToString());
-                //                Log.Close();
-                //            }
-                //        }
-                //    }
-                //    catch (Exception e)
-                //    {
-                //        using (StreamWriter Log = new StreamWriter(UrlLog, true))
-                //        {
-                //            Log.WriteLine(".- Se produjo una Exception");
-                //            Log.WriteLine(".- Mensaje: " + e.Message);
-                //            Log.Close();
-                //        }
-                //    }
+                    ////Timbrar comprobante KAVAK
+                    var respuestaTimbrado = TimbrarComprobanteKAVAK(Utilerias.AESEncrypt(Layout), Utilerias.AESEncrypt(Usuario), Utilerias.AESEncrypt(Password));
 
-                //}
+                    if (respuestaTimbrado.GetValue("exitoso").ToString() == "true")
+                    {
+                        using (StreamWriter Log = new StreamWriter(UrlLog, true))
+                        {
+                            Log.WriteLine(".- Timbrado con exito");
+                            Log.Close();
+                        }
 
-                //Obtener PDF
-                //ObtenerPDFComprobante()
+                        string XMLTimbrado = respuestaTimbrado.GetValue("XmlTimbrado").ToString();
 
-                //Cancelar Comprobante
-                //CancelarComprobante()
+                        //KAVAK
+                        XMLTimbrado = Utilerias.AESDecrypt(XMLTimbrado);
 
-
-                //------- Timbrar Comprobante KAVAK
-
-                TimbrarComprobanteKAVAK(File.ReadAllText("C:/Entrada/Lineas.txt"), File.ReadAllText("C:/Entrada/Usuario.txt"), File.ReadAllText("C:/Entrada/Password.txt"), Token);
-
+                        File.WriteAllText(UrlXML, XMLTimbrado);
+                        using (StreamWriter Log = new StreamWriter(UrlLog, true))
+                        {
+                            Log.WriteLine(".- Se crea xml en la carpeta");
+                            Log.Close();
+                        }
+                    }
+                    else
+                    {
+                        using (StreamWriter Log = new StreamWriter(UrlLog, true))
+                        {
+                            Log.WriteLine(".- Error al timbrar");
+                            Log.WriteLine(".- Mensaje: " + respuestaTimbrado.GetValue("mensajeError").ToString());
+                            Log.WriteLine(".- Codigo Error: " + respuestaTimbrado.GetValue("codigoError").ToString());
+                            Log.Close();
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    using (StreamWriter Log = new StreamWriter(UrlLog, true))
+                    {
+                        Log.WriteLine(".- Se produjo una Exception");
+                        Log.WriteLine(".- Mensaje: " + e.Message);
+                        Log.Close();
+                    }
+                }
 
             }
+
+            //Obtener PDF
+            //ObtenerPDFComprobante()
+
+            //Cancelar Comprobante
+            //CancelarComprobante()
         }
+
+
 
 
         public static string ValidarAcceso(string Usuario, string Password)
@@ -134,6 +138,7 @@ namespace TestWebApiREST
             return JObject.Parse(response.Content);
         }
 
+        //Metodo para enviar los parametros por el header de la solicitud
         //public static JObject TimbrarComprobanteKAVAK(string Layout, string Usuario, string Password, string Token)
         //{
         //    var client = new RestClient(ConfigurationManager.AppSettings["UrlTimbrarComprobanteKAVAK"]);
@@ -151,10 +156,11 @@ namespace TestWebApiREST
 
         //}
 
-        public static JObject TimbrarComprobanteKAVAK(string Layout, string Usuario, string Password, string Token)
+        //Metodo para enviar los parametros por el cuerpo de la solicitud
+        public static JObject TimbrarComprobanteKAVAK(string Layout, string Usuario, string Password/*, string Token*/)
         {
             dynamic clase = new ExpandoObject();
-            clase.Usuario = Usuario; 
+            clase.Usuario = Usuario;
             clase.Password = Password;
             clase.Lineas = Layout;
 
@@ -163,7 +169,7 @@ namespace TestWebApiREST
             var client = new RestClient(ConfigurationManager.AppSettings["UrlTimbrarComprobanteKAVAK"]);
             client.Timeout = -1;
             var request = new RestRequest(Method.POST);
-            request.AddHeader("Authorization", "Bearer " + Token);
+            //request.AddHeader("Authorization", "Bearer " + Token);
             request.AddHeader("Content-Type", "application/json");
             request.AddParameter("application/json", body, ParameterType.RequestBody);
             IRestResponse response = client.Execute(request);
---
+++ TestWebApiREST/Utilerias.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWebApiREST
+{
+    public static class Utilerias
+    {
+        private static string KeyEcrypt = ConfigurationManager.AppSettings["KeyEcrypt"];
+        public static string AESEncrypt(string base64Encode)
+        {
+            string decode = null;
+            try
+            {
+                using (var aes = new AesCryptoServiceProvider())
+                {
+                    aes.Key = Encoding.UTF8.GetBytes(KeyEcrypt);
+                    aes.Mode = CipherMode.ECB;
+                    aes.Padding = PaddingMode.PKCS7;
+                    //aes.FeedbackSize = aes.BlockSize;                                
+                    byte[] encrypted = AESCrypto(CryptoOperation.ENCRYPT, aes, Encoding.UTF8.GetBytes(base64Encode));
+                    return Convert.ToBase64String(encrypted);
+                }
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+            return decode;
+        }
+        public static string AESEncrypt(byte[] doc)
+        {
+            string decode = null;
+            try
+            {
+                using (var aes = new AesCryptoServiceProvider())
+                {
+                    aes.Key = Encoding.UTF8.GetBytes(KeyEcrypt);
+                    aes.Mode = CipherMode.ECB;
+                    aes.Padding = PaddingMode.PKCS7;
+                    //aes.FeedbackSize = aes.BlockSize;                                
+                    byte[] encrypted = AESCrypto(CryptoOperation.ENCRYPT, aes, doc);
+                    return Convert.ToBase64String(encrypted);
+                }
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+            return decode;
+        }
+        public static string AESDecrypt(string base64Encode)
+        {
+            string decode = null;
+            try
+            {
+                using (var aes = new AesCryptoServiceProvider())
+                {
+                    aes.Key = Encoding.UTF8.GetBytes(KeyEcrypt);
+                    aes.Mode = CipherMode.ECB;
+                    aes.Padding = PaddingMode.PKCS7;
+                    //aes.FeedbackSize = aes.BlockSize;                                                    
+                    byte[] decrypted = AESCrypto(CryptoOperation.DECRYPT, aes, Convert.FromBase64String(base64Encode));
+                    return Encoding.UTF8.GetString(decrypted);
+                }
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+            return decode;
+        }
+
+        public static byte[] AESCrypto(CryptoOperation cryptoOperation, AesCryptoServiceProvider aes, byte[] message)
+        {
+            using (var memStream = new MemoryStream())
+            {
+                CryptoStream cryptoStream = null;
+
+                if (cryptoOperation == CryptoOperation.ENCRYPT)
+                    cryptoStream = new CryptoStream(memStream, aes.CreateEncryptor(), CryptoStreamMode.Write);
+                else if (cryptoOperation == CryptoOperation.DECRYPT)
+                    cryptoStream = new CryptoStream(memStream, aes.CreateDecryptor(), CryptoStreamMode.Write);
+
+                if (cryptoStream == null)
+                    return null;
+
+                cryptoStream.Write(message, 0, message.Length);
+                cryptoStream.FlushFinalBlock();
+                return memStream.ToArray();
+            }
+        }
+
+        public enum CryptoOperation
+        {
+            ENCRYPT,
+            DECRYPT
+        };
+    }
+}
